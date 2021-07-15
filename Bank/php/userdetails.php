ce7fadbc050a459bc69fbfd90e72097244d16134
Ehteshamali-889 <63139751+Ehteshamali-889@users.noreply.github.com>
2021-07-15 18:26:01 +0500

    Update userdetails.php

--- Bank/php/userdetails.php
+++ Bank/php/userdetails.php
@@ -118,9 +118,10 @@
         </form>
     </div>
     </div>
-    <footer class="center footer">
-        <h6 class="info"><b>2021 © Made by Manasi Wader</b></h6>
-        The Sparks Foundation
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Made By Ehtesham Ali With Love - The Sparks Foundation</span>
+      </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
